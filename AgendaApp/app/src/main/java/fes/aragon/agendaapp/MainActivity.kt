71f5283050cbb438f7378678e1270e5889747800
osvaldo <osvaldo.guerra.c008@gmail.com>
2023-05-08 18:11:09 -0600

    create proyect

---
+++ AgendaApp/app/src/main/java/fes/aragon/agendaapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package fes.aragon.agendaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
